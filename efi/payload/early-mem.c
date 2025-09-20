e228e37be6f4f664c34719b5d7e557abee5b7176
Chali Anis <chalianis1@gmail.com>
2025-09-19 20:09:48 -0400

    efi: payload: earlymem: allocate only the barebox needs in term of memory.
    Since an EFI malloc callback is available to provide additional memory
    on demand, limit early allocations to what barebox strictly requires.
    This ensures more deterministic behavior and reduces the risk of OOM
    conditions.
    
    Signed-off-by: Chali Anis <chalianis1@gmail.com>

--- efi/payload/early-mem.c
+++ efi/payload/early-mem.c
@@ -6,27 +6,17 @@
 #include <efi/efi-payload.h>
 
 void *efi_earlymem_alloc(const struct efi_system_table *sys_table,
-			 size_t *memsize, enum efi_memory_type mem_type)
+			 size_t memsize, enum efi_memory_type mem_type)
 {
 	struct efi_boot_services *bs = sys_table->boottime;
 	efi_physical_addr_t mem;
 	efi_status_t efiret;
-	size_t m_sz;
 
-	if (IS_ENABLED(CONFIG_X86))
-		m_sz = SZ_512M;
-	else
-		m_sz = SZ_256M;
-
-	for (*memsize = m_sz; *memsize >= SZ_8M; *memsize /= 2) {
-		efiret  = bs->allocate_pages(EFI_ALLOCATE_ANY_PAGES, mem_type,
-					     *memsize / EFI_PAGE_SIZE, &mem);
-		if (!EFI_ERROR(efiret) || efiret != EFI_OUT_OF_RESOURCES)
-			break;
-	}
+	efiret  = bs->allocate_pages(EFI_ALLOCATE_ANY_PAGES, mem_type,
+					     memsize / EFI_PAGE_SIZE, &mem);
 	if (EFI_ERROR(efiret))
 		panic("failed to allocate %zu byte memory pool: 0x%lx\n",
-		      *memsize, efiret);
+		      memsize, efiret);
 
 	return efi_phys_to_virt(mem);
 }

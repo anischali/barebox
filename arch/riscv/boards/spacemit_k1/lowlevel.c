92eb6c542c6154cbba8e609ccacff01cacc0abac
Chali Anis <chalianis1@gmail.com>
2026-03-14 01:25:02 -0400

    wip

---
+++ arch/riscv/boards/spacemit_k1/lowlevel.c
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <common.h>
+#include <asm/barebox-riscv.h>
+#include <debug_ll.h>
+#include <asm/riscv_nmon.h>
+
+ENTRY_FUNCTION(start_spacemit_k1, a0, a1, a2)
+{
+	extern char __dtb_z_spacemit_k1_start[];
+	void *fdt;
+
+	/* On POR, we are running from read-only memory here. */
+
+	fdt = __dtb_z_spacemit_k1_start + get_runtime_offset();
+
+	barebox_riscv_supervisor_entry(0x80000000, SZ_128M, a0, fdt);
+
+}
